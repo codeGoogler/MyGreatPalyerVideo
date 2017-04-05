d631503a97150a7cbcb8eb73dfc694f374504437
androidstarjack <yyhkack1302@163.com>
2017-04-05 23:41:05 +0800

    音视频

---
+++ app/src/main/java/cv/yyh/com/mygreatviedeo/CustomView/JCVideoPlayerStandardAutoCompleteAfterFullscreen.java
@@ -0,0 +1,30 @@
+package cv.yyh.com.mygreatviedeo.CustomView;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import fm.jiecao.jcvideoplayer_lib.JCVideoPlayerStandard;
+
+/**
+ * Created by Nathen on 2016/11/26.
+ */
+
+public class JCVideoPlayerStandardAutoCompleteAfterFullscreen extends JCVideoPlayerStandard {
+    public JCVideoPlayerStandardAutoCompleteAfterFullscreen(Context context) {
+        super(context);
+    }
+
+    public JCVideoPlayerStandardAutoCompleteAfterFullscreen(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void onAutoCompletion() {
+        if (currentScreen == SCREEN_WINDOW_FULLSCREEN) {
+            setUiWitStateAndScreen(CURRENT_STATE_AUTO_COMPLETE);
+        } else {
+            super.onAutoCompletion();
+        }
+
+    }
+}
